29a558b87c6f9f35a8cf5e03a5c6c3bdf99cfe79
Cris Mihalache <me@f3rno.com>
2020-05-25 18:12:55 +0700

    (wip)

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict'
 
+/**
+ * Simple callback queue
+ *
+ * @class
+ * @module cbq-js
+ * @license MIT
+ */
 class CbQ {
   constructor () {
     this.q = new Map()
   }
 
+  /**
+   * Adds a callback to a named queue, creating it if it does not exist.
+   *
+   * @param {string} k - queue key/ID
+   * @param {Function} cb - callback to add
+   */
   push (k, cb) {
     if (!this.q.has(k)) {
       this.q.set(k, [])
@@ -14,6 +27,14 @@ class CbQ {
     q.push(cb)
   }
 
+  /**
+   * Calls every callback in a named queue with the provided parameters if it
+   * is found.
+   *
+   * @param {string} k - queue key/ID
+   * @param {Error} err - first argument to callbacks
+   * @param {*} res - second argument to callbacks
+   */
   trigger (k, err, res) {
     const q = this.q.get(k)
 
@@ -23,6 +44,12 @@ class CbQ {
     q.forEach(cb => cb(err, res))
   }
 
+  /**
+   * Returns the length of the named queue
+   *
+   * @param {string} k - queue key/ID
+   * @returns {number} length
+   */
   cnt (k) {
     const q = this.q.get(k)
     if (!q) return 0
